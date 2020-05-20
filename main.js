7605be96aa02b65931545b436bb42e05b096e359
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-20 16:53:04 +0200

    inizio

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    // Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
+    // https://flynn.boolean.careers/exercises/api/array/music
+    // Ciclare quindi i dischi e ottenuti e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+});
