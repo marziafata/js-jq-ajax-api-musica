2462c0e4c5a9178f5804c61bd0d87a8279ac0b1e
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-21 15:42:42 +0200

    fine esercizio + commenti

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
 $(document).ready(function() {
+
 // Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
 // https://flynn.boolean.careers/exercises/api/array/music
-// Ciclare quindi i dischi e ottenuti e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
-var source = $('#entry-template').html();
-var template = Handlebars.compile(source);
-
-var context = {
-    //gli oggetti disco che sono dentro ajax
-};
-
-var disco_cover = template(context);
-
-$('cds-container.container').append(disco_cover);
-
-// var cover = [];
-// console.log('cover: ' + cover);
-
 $.ajax({
     'url' : "https://flynn.boolean.careers/exercises/api/array/music",
     'method' : 'GET',
     'success' : function(data) {
+        //dichiaro le variabili che mi serviranno con Handlebars
+        var source = $('#entry-template').html();
+        var template = Handlebars.compile(source);
 
-                    var dischi = data.response;
-                    console.log(dischi);
+        //leggo il contenuto dell'api (un array di dischi con determinate proprietà)
+        var dischi = data.response;
 
-                    for (var i = 0; i < dischi.length; i++) {
-                        var dischi_singoli = dischi[i]
-                        cover.push(dischi_singoli);
-                    }
-                }, //fine function(data) di success
+        // Ciclare quindi i dischi e ottenuti...
+        for (var i = 0; i < dischi.length; i++) {
+            var disco = dischi[i];
+
+            // imposto le proprietà dell'oggetto context e le compilo con le proprietà recuperate da ogni disco
+            var context = {
+                "copertina" : disco.poster,
+                "titolo" : disco.title,
+                "autore" : disco.author,
+                "anno" : disco.year
+            };
+
+            //compilo il template con le proprietà inserite dentro context
+            var disco_cover = template(context);
+
+            //...e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+            $('.cds-container.container').append(disco_cover);
+        }//fine ciclo for
+
+    }, //fine function(data) di success
     'error' : function() {
         alert('qualcosa è andato storto...');
+
     }//fine function di error
 });//fine ajax
-
 
 
 });//fine document ready
